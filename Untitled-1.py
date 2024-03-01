02a89b4640179b00ea1b197e465f9b55855a977c
unknown <jassoangel3010@gmail.com>
2024-03-01 17:45:40 -0600

    cambio angel

---
+++ Untitled-1.py
@@ -0,0 +1,11 @@
+nombre = input("ingresa tu nombre")
+print(nombre)
+apellido = input("ingresa tu apellido")
+print(apellido)
+edad = input("ingresa tu edad")
+print(edad)
+inicial_nombre = nombre[0] .upper()
+iniciar_apellido = apellido[0] .upper()
+ooo= "💨🕳️"
+curp = f"{iniciar_apellido}{inicial_nombre}{edad}{ooo}"
+print(f"tu curp es {curp}")
